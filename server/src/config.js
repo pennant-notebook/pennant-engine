b4e72ee715bcc47aa818dc555b191cd99c3d77a5
mricasa <michaelr624@gmail.com>
2023-07-24 05:13:46 +0000

    dirname compatible with different app start contexts

--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 module.exports = {
   PORT: process.env.PORT || 3002,
