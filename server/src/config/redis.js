4d81c92bc6fb3214a115171f31671fb15aaf780c
mricasa <michaelr624@gmail.com>
2023-07-28 03:57:12 +0000

    redis reconnection logic

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,7 +1,7 @@
 const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = require("../config");
 const redis = require("redis");
 
-const client = redis.createClient({
+const clientOptions = {
   legacyMode: true,
   socket: {
     // host: "127.0.0.1",
@@ -9,15 +9,42 @@ const client = redis.createClient({
     // ! When running server on host machine, use 127.0.0.1
     host: REDIS_HOST,
     port: REDIS_PORT,
+    reconnectStrategy (retries) {
+      if (retries > 10) {
+        console.log("Too many retries on REDIS. Connection Terminated");
+        return new Error("Too many retries.");
+      } else {
+        return retries;
+      }
+    },
   },
   password: REDIS_PASSWORD,
-});
 
-client.connect().then(
 
-  client.on("error", (err) => {
-    console.log("Error " + err);
-  })
-);
+}
+
+
+let client;
+
+(async () => {
+  client = redis.createClient(clientOptions);
+
+  client.on("connect", () => {
+    console.log("Connected to Redis!");
+  });
+
+  client.on("error", (error) => {
+    console.error(`Redis Error : ${error}`)
+    process.exit(1);
+  });
+
+  await client.connect();
+})();
+
+
+
+// client.on("error", (err) => {
+//   console.log("Error " + err);
+// })
 
 module.exports = { client }
